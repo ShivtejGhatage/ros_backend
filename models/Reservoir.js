ede48e1b6dfc2d6f9ffab95f5e860359ec523f63
Shivtej Ghatage <shivtejmailbox@gmail.com>
2025-05-23 21:22:52 +0530

    Reservoir Changes

--- models/Reservoir.js
+++ models/Reservoir.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 
 const ReservoirSchema = new mongoose.Schema({
   name: { type: String, required: true , unique: true},
-  dam: { type: Schema.Types.ObjectId, ref: 'Dam', required: true },
+  sectionOffice: { type: Schema.Types.ObjectId, ref: 'SectionOffice', required: true },
   subdam: { type: Schema.Types.ObjectId, ref: 'Subdam', required: true },
   alertL: { type: Number, required: true },
   dangerL : { type: Number, required: true },
--- models/User.js
+++ models/User.js
@@ -5,7 +5,7 @@ const Schema = mongoose.Schema;
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  designation: { type: String, required: true, enum: ['Canal Inspector', 'Superior', 'Superior\'s Superior'] },  // Can be 'admin', 'user', or 'manager' (customize as needed)
+  designation: { type: String, required: true, enum: ['कार्यकारी अभियंता', 'उपविभागीय अभियंता', 'शाखा अभियंता','कालवा निरीक्षक'] },  // Can be 'admin', 'user', or 'manager' (customize as needed)
   phoneNumber: { type: String, required: true },
 });
 
